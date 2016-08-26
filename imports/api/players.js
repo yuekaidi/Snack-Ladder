d0840d6f4064206fa575b38cc2826ecf4bc2e143
Kaidi <kaidi.yue@gmail.com>
2016-08-26 11:50:02 +0800

    First Commit

---
+++ imports/api/players.js
@@ -0,0 +1,4 @@
+import { Mongo } from 'meteor/mongo';
+
+Players = new Mongo.Collection('players');
+
---
+++ imports/ui/body.js
@@ -0,0 +1,58 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Mongo } from 'meteor/mongo';
+
+Players = new Mongo.Collection('players');
+
+//import { Players } from '../api/players.js';
+
+import './body.html';
+
+Template.body.events({
+  'submit .new-player'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Players.insert({
+      text,
+      score: 0,
+      position: 0, // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
+  },
+});
+
+
+
+Template.play.onCreated(function playOnCreated() {
+  // counter starts at 0
+  this.number = new ReactiveVar(0);
+});
+
+Template.play.helpers({
+  number() {
+    return Template.instance().number.get();
+  },
+});
+
+Template.play.events({
+  'click button'(event, instance) {
+    // increment the counter when button is clicked
+    instance.number.set(Math.floor(Math.random() * 6) + 1  );
+  },
+});
+
+/*Template.body.helpers({
+  players() {
+    return Players.find({}, { sort: { createdAt: -1 } });
+  },
+});*/
